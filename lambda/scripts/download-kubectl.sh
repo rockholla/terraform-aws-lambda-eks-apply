da2e751093c911ad85cf2a43902219c290a58225
Patrick Force <patrickforce@gmail.com>
2026-02-06 08:29:32 -0700

    contd work on lambda sub-component

--- lambda/scripts/download-kubectl.sh
+++ lambda/scripts/download-kubectl.sh
@@ -3,5 +3,6 @@
 set -eo pipefail
 
 this_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$this_dir"
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x "${this_dir}/kubectl"
+chmod +x ./kubectl
