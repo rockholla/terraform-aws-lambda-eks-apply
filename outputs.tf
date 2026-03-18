fdeca6ef90715351b2af8df87da86b72a2d8d1d1
Marc Barlo <mbarlo@twilio.com>
2026-03-18 14:46:23 -0700

    add lambda role arn output (#23)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "invocation_log" {
   description = "Log of the Lambda invocation execution"
   value       = jsondecode(aws_lambda_invocation.manifest_apply.result).body
 }
+
+output "lambda_role_arn" {
+  description = "ARN of the IAM role used by the Lambda function"
+  value       = aws_iam_role.lambda.arn
+}
